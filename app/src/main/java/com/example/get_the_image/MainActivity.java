6821c330e6b9d2e28ffc164ef2466d4910c4af4a
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-18 22:52:55 +0530

    configured butterknife

--- app/src/main/java/com/example/get_the_image/MainActivity.java
+++ app/src/main/java/com/example/get_the_image/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.get_the_image;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
