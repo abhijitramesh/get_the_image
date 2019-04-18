e586aa13bdf61c7b52988eeac03a0fadf087df75
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-18 22:59:31 +0530

    Binded the Edit Text with ButterKnife

--- app/src/main/java/com/example/get_the_image/MainActivity.java
+++ app/src/main/java/com/example/get_the_image/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.get_the_image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-
+@BindView(R.id.res1)
+    EditText res1;
+@BindView(R.id.res2)
+    EditText res2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
